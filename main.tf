262fdcc010cfc2962890d4a65da0c3ee5859af48
Abdul Ahad Khan <ahadaak231@gmail.com>
2024-03-15 17:15:28 +0400

    cloud distribution dns tables done and cicd setup

--- main.tf
+++ main.tf
@@ -51,13 +51,6 @@ resource "aws_s3_bucket_website_configuration" "portfolio_bucket_website" {
   }
 
 }
-data "aws_iam_user" "runner" {
-  user_name = "aak231-github-runner"
-}
-data "aws_iam_user" "main" {
-  user_name = "aak231"
-}
-
 resource "aws_s3_bucket_policy" "allow_access_from_current_account" {
   bucket = aws_s3_bucket.portfolio_bucket.id
 
@@ -179,5 +172,16 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     acm_certificate_arn      = aws_acm_certificate.cert.arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
+  }
+}
+resource "aws_route53_record" "cloudfront_distribution" {
+  zone_id = data.aws_route53_zone.portfolio.zone_id
+  name    = "ahadkhans.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
   }
 }
